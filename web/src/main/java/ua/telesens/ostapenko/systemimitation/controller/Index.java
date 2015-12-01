85fd23e3bf8a25b67e4e3468c9b77d5cdc2c245b
root <truegreg13@mail.ru>
2015-12-01 19:07:56 +0200

    Delete annotation request method from class in home controller.

--- web/src/main/java/ua/telesens/ostapenko/systemimitation/controller/Index.java
+++ web/src/main/java/ua/telesens/ostapenko/systemimitation/controller/Index.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import static ua.telesens.ostapenko.systemimitation.controller.EnumView.*;
+import static ua.telesens.ostapenko.systemimitation.controller.EnumView.redirectTo;
 
 /**
  * @author root
@@ -13,10 +13,9 @@ import static ua.telesens.ostapenko.systemimitation.controller.EnumView.*;
  */
 @Slf4j
 @Controller
-@RequestMapping({"/index"})
 public class Index {
 
-    @RequestMapping()
+    @RequestMapping(value = "/index")
     public ModelAndView home() {
         log.debug("Rendering /index page. --> redirect to \\/: ");
         return new ModelAndView(redirectTo(Home.VIEW_NAME_HOMEPAGE));

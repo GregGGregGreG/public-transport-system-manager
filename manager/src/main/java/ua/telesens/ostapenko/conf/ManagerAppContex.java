7de26a395c3dff0afd7560a39357b1b172550c93
root <truegreg13@mail.ru>
2015-12-04 19:41:09 +0200

    Added empty manager application context

---
+++ manager/src/main/java/ua/telesens/ostapenko/conf/ManagerAppContex.java
@@ -0,0 +1,11 @@
+package ua.telesens.ostapenko.conf;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author root
+ * @since 03.12.15
+ */
+@Configuration
+public class ManagerAppContex {
+}
